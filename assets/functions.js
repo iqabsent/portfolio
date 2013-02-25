791dcbe550f168a72a92c5949d8ad43e0786fe5f
Willem B. van der Merwe <iqabsent@gmail.com>
2013-02-25 13:00:09 +0000

    work in progress..

--- assets/functions.js
+++ assets/functions.js
@@ -1,14 +1,19 @@
-$(document).ready(function(){
+// this is all jQuery
+$(document).ready(function(){ // this runs as soon as the page has loaded
 
+  // load the hello page into the page container (the div with id=page)
   $("#page").load("pages/hello.html");
 
-  $("nav li a").click(function(e){
-    //if($(this).parent().is("li:first-child")) return;
-    e.preventDefault();
+  // navigation menu stuff
+  $("nav li a").click(function(e){  // a click on <a> inside <li> inside <nav>
+    e.preventDefault(); // stop the link from firing
+    // change class (CSS) to mark clicked item as active
     $(this).parent().addClass("active").siblings().removeClass("active");
+    // load via an Ajax call .load(), into the element (a <div>) with id set to
+    // "page", the URL (href) of $(this), this being the link which was clicked
     $("#page").load($(this).attr("href"), function(response, status){
-      $("#page").css('height','auto');
-      if(status == "error") {
+      $("#page").css('height','auto');  // adjust page height
+      if(status == "error") { // load the 404 page if there's an error
         $("#page").load("pages/404.html");
       }
     });
