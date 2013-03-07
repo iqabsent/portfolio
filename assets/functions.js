d1373c84d9f28c9a180f7559f03ada84f2180af2
Willem B. van der Merwe <iqabsent@gmail.com>
2013-03-07 02:55:45 +0000

    fixed # links for mobile. added capacity filters

--- assets/functions.js
+++ assets/functions.js
@@ -1,49 +1,56 @@
 // this is all jQuery
 $(document).ready(function(){ // this runs as soon as the page has loaded
 
-  // navigation menu
-  $("nav li a").click(function(e){    // a click on <a> inside <li> inside <nav>
-    var target = $(this).attr('id');  // get id of item clicked
-    // trick to stop setting the hash from scrolling the page
-    $('html').attr('id', target);     // set id of <html> to target
-    // without trick, this does unwanted srolling
-    window.location.hash = target;    // set on-page/local link (the # bit of url)
-    $('html').attr('id', "");         // undo the trick
-    e.preventDefault();               // stop the link from firing
-    // .. because we changed the window.location.hash, the hash change event will
-    // fire which we set up a listener for
-  });
-  
-  // hash change listener
-  $(window).on('hashchange', function() {
-    // check for top links
-    if(window.location.hash == "#top") {
-      // restore correct hash
-      window.location.hash = $('#page').children(':first-child').attr('id');
-      return;
-    }
-    // calls loadPage when the hash change event fires
-    loadPage(window.location.hash);
-  });
-  
-  // load page into #page container from href in nav menu item with id matching hash
-  function loadPage(hash) {
+  // a utility function to load page content via ajax
+  function loadPage(menu_id) {
+    var $menu_item = $('#' + menu_id);         // find menu item matching id
+    var path = $menu_item.attr('href'); // get it's href
     // change class (CSS) to mark clicked item as active, and others as not
-    $(hash).parent().addClass("active").siblings().removeClass("active");
-    // find menu item with id matching hash and load href into page container
-    var page = $(hash).attr("href");
-    $("#page").load(page, function(response, status){
+    $menu_item.parent().addClass("active").siblings().removeClass("active");  
+    // and load href into page container
+    $("#page").load(path, function(response, status){
       // when that's done..
       $("#page").css('height','auto');  // adjust page height
-      window.scrollTo(0, 0);  // scroll to top
-      if(status == "error") { // load the 404 page if there's an error
+      window.scrollTo(0, 0);            // scroll to top
+      if(status == "error") {           // load 404 page if there's an error
         $("#page").load("pages/404.html");
       }
     });
   }
+
+  // navigation menu
+  $("nav li a").click(function(e){  // a click on <a> inside <li> inside <nav>
+    e.preventDefault();             // stop the link from firing
+    var target = $(this).attr('id');
+    setHash(target);                // set hash
+    loadPage(target);               // load the page
+  });
   
+  // hash change listener(s .. for compatibility)
+  if (window.addEventListener) {
+    window.addEventListener("hashchange", hashChange, false);
+  } else if (window.attachEvent) {
+    window.attachEvent("onhashchange", hashChange);
+  }
+  function hashChange() {
+    if(window.location.hash == "#top")
+      setHash(currentPage().replace('-page',''));    
+  }
+  
+  function setHash(hash) {
+    // trick to stop the page from scrolling when setting the hash
+    $('html').attr('id', hash);   // set id of <html> to target
+    // without trick, this next line does unwanted srolling
+    window.location.hash = hash;  // set on-page/local link (the # bit of url)
+    $('html').attr('id', "");     // undo the trick
+  }
+  
+  function currentPage() {
+    return $('#page').children(':first-child').attr('id');
+  }
+ 
   // check for a local hash link (if any) or use "hello"
-  var hash = window.location.hash || "#hello";
+  var hash = window.location.hash.replace('#','') || "hello";  
   loadPage(hash); // load the page
   
   // filtering by tag
