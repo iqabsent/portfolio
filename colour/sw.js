1853d39ce4f316625485a267f007a7c2367abc0d
Willem van der Merwe <iqabsent@gmail.com>
2026-05-17 19:52:44 +0100

    Publish colour filter test

---
+++ colour/sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'boulder-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
